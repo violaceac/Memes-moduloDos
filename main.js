1d4511044f7bf56973e54c5aca737a2f226cb392
Violeta Contreras <viol.contreras@gmail.com>
2024-12-18 04:18:06 -0300

    todo listo excepto filtros del panel imagen que sigue fallando, falta responsive

--- main.js
+++ main.js
@@ -8,13 +8,12 @@ window.onload = () => {
 
 
 
-
+const $body = $("#body");
 // ========= HEADER ==========
 
 //  BOTONES PARA DESPLEGAR PANEL IMAGEN/TEXTO
 const $buttonImagen = $("#botonImagen");
 const $buttonTexto = $("#botonTexto"); 
-
 //  BOTONES MODO CLARO Y OSCURO 
 const $modoOscuro = $("#botonModoOscuro");
 const $modoClaro = $("#botonModoClaro");
@@ -27,28 +26,22 @@ const $modoClaro = $("#botonModoClaro");
 const $h2Top = $("#textoTop");
 const $h2Bottom = $("#textoBottom");
 // IMAGEN
-const $imagenMeme = $("#imagen");
+const $imagenMeme = $("#contenedor-imagen");
+const $memeCompleto = $("#contenedor-meme")
 // BOTONES
 const $buttonDescarga = $("#botonDescarga");
-const $buttonResetTodo= $("#botonReestablecer")
 
 
 
 // ========== ASIDE IMAGEN ===========
 
 const $asideImagen = $("#panelImg");
-
 // IMAGEN
 const $inputURL = $("#URL");
 //FONDO
 const $inputColorFondo = $("#colorFondo");
-// SELECT
-const $selectNinguno = $("#opcionNinguno");
-const $selectAclarar = $("#opcionAclarar");
-const $selectOscurecer = $("#opcionObscurecer");
-const $selectDiferencia = $("#opcionDiferencia");
-const $selectLuminosidad = $("#opcionLuminosidad");
-const $selectMultiplicar = $("#opcionMultiplicar");
+// SELECT MEZCLA
+const $selectMezcla = $("#select-mezcla")
 // FILTROS
 const $rangeBrillo = $("#filtroBrillo");
 const $rangeOpacidad = $("#filtroOpacidad");
@@ -64,21 +57,16 @@ const $buttonResetFiltros = $("#resetFiltros");
 
 
 
-
-
 // ========== ASIDE TEXTO ===========
 
 const $asideTexto = $("#panelTexto");
-
 // TEXTO SUPERIOR
 const $inputTextoSup = $("#textoSuperior");
 const $checkSinTextoSup = $("#sinTextoSup");
 // TEXTO INFERIOR
 const $inputTextoInf = $("#textoInferior");
 const $checkSinTextoInf = $("#sinTextoInf");
-
 // FUENTE
-
 // --- TIPO DE FUENTE
 const $selectFuente = $("#inputFuentes");
 // --- TAMAÑO DE FUENTE
@@ -87,183 +75,267 @@ const $inputTamañoFuente = $("#inputTamaño");
 const $buttonIzquierda = $("#izquierda");
 const $buttonCentro = $("#centro");
 const $buttonDerecha = $("#derecha");
-
 // COLOR
 const $inputColorFuente = $("#colorFuente");
 // FONDO
 const $inputFondo = $("#fondoFuente");
 const $checkTransparente = $("#checkTransparente");
-
 //CONTORNO
 const $buttonNinguno = $("#ninguno");
 const $buttonClaro = $("#claro");
 const $buttonOscuro = $("#oscuro");
-
 // ESPACIADO
 const $inputEspaciado = $("#espaciadoFuente");
-
 //INTERLINEADO
-const $selectCeroOcho = $("#ceroOcho");
-const $selectUno = $("#uno");
-const $selectUnoDos = $("#unoDos");
-const $selectUnoCinco = $("#unoCinco");
-const $selectDos = $("#dos");
-const $selectDosCinco =$("#dosCinco");
+const $selectInterlineado = $("#inputInterlineado")
 
 // ================ FIN DE ELEMENTOS HTML ===============
 
 
 
+// ----------------------------------------------------
 
 
-// ============== FUNCIONES ========================
 
+// ============== EVENTOS ========================
 
 // ==== HEADER ====
-
-//  DESPLEGAR PANEL IMAGEN/TEXTO
+//  DESPLEGAR PANEL TEXTO
 $buttonTexto.addEventListener("click", () => {
     $asideTexto.style.display = "flex";
     $asideImagen.style.display = "none"
 })
-
+//  DESPLEGAR PANEL IMAGEN
 $buttonImagen.addEventListener("click", () => {
     $asideImagen.style.display = "flex";
     $asideTexto.style.display = "none"
 })
-
-// BOTON CLARO/OSCURO
+// BOTON MODO OSCURO
 $modoOscuro.addEventListener("click", () => {
-    $modoClaro.style.display = "flex";
-    $modoOscuro.style.display = "none"
+    if($body.classList.contains("modo-claro")) {
+        $body.classList.remove("modo-claro")
+        $body.classList.add("modo-oscuro");
+        $modoOscuro.style.display = "none";
+        $modoClaro.style.display = "flex"
+    }
 })
-
+// BOTON MODO CLARO
 $modoClaro.addEventListener("click", () => {
-    $modoOscuro.style.display = "flex";
-    $modoClaro.style.display = "none"
+    if($body.classList.contains("modo-oscuro")) {
+        $body.classList.remove("modo-oscuro")
+        $body.classList.add("modo-claro");
+        $modoOscuro.style.display = "flex";
+        $modoClaro.style.display = "none"
+    }
 })
-
-// ---------------------------------------
-
-// ==== ASIDES ====
+// ========== FIN HEADER ============
 
 
-// ====== ASIDE IMAGEN ======
 
+// ==== MAIN ====
+// BOTON DE DESCARGA
+$buttonDescarga.addEventListener("click", (e) => {
+    console.log(e)
+    domtoimage.toBlob($memeCompleto).then((blob) => {
+        saveAs(blob, "my-meme.png");
+      }).catch((error)=> {console.log(error)});
+})
+// ====== FIN MAIN =========
+
+
+
+// ========== ASIDES ==========
+
+// = ASIDE IMAGEN =
 //INPUT URL
 $inputURL.addEventListener("input", () => {
-    $imagenMeme.src = $inputURL.value
+   $imagenMeme.style.backgroundImage = `url(${$inputURL.value})`
+})
+// FONDO
+$inputColorFondo.addEventListener("input", () => {
+    $imagenMeme.style.backgroundColor = `${$inputColorFondo.value}`
+})
+//MEZCLA
+$selectMezcla.addEventListener("change", () => {
+    $imagenMeme.style.backgroundBlendMode = $selectMezcla.value
 })
 
-// FILTROS-------- cada vez que modifico un rango se deshace el anterior
+
+// FILTROS NO SE COMPORTA COMO ES ESPERADO, PROBANDO FUNCIONES O ACUMULAR LOS VALORES EN OTRA VARIABLE PARA QUE NO SE REPITAN(?)
+
+// FILTROS *ya no se sobreescriben, pero ahora no refresca su propio valor, si le bajo el brillo luego no sube
 $rangeBrillo.addEventListener("input", () => {
-    $imagenMeme.style.filter = `brightness(${$rangeBrillo.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} brightness(${$rangeBrillo.value}) `;
 })
 $rangeOpacidad.addEventListener("input", () => {
-    $imagenMeme.style.filter = `opacity(${$rangeOpacidad.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} opacity(${$rangeOpacidad.value}) `
 })
 $rangeContraste.addEventListener("input", () => {
-    $imagenMeme.style.filter = `contrast(${$rangeContraste.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} contrast(${$rangeContraste.value}%) `
 })
 $rangeDesenfoque.addEventListener("input", () => {
-    $imagenMeme.style.filter = `blur(${$rangeDesenfoque.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} blur(${$rangeDesenfoque.value}px) `
 })
 $rangeGrises.addEventListener("input", () => {
-    $imagenMeme.style.filter = `grayscale(${$rangeGrises.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} grayscale(${$rangeGrises.value}%) `
 })
 $rangeSepia.addEventListener("input", () => {
-    $imagenMeme.style.filter = `sepia(${$rangeSepia.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} sepia(${$rangeSepia.value}%) `
 })
 $rangeHue.addEventListener("input", () => {
-    $imagenMeme.style.filter = `hue-rotation(${$rangeHue.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} hue-rotate(${$rangeHue.value}deg) `
 })
 $rangeSaturado.addEventListener("input", () => {
-    $imagenMeme.style.filter = `saturation(${$rangeSaturado.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} saturate(${$rangeSaturado.value}%) `
 })
 $rangeNegativo.addEventListener("input", () => {
-    $imagenMeme.style.filter = `invert(${$rangeNegativo.value})`
+    let aux = $imagenMeme.style.filter
+    $imagenMeme.style.filter = `${aux} invert(${$rangeNegativo.value}) `
 })
-//---------------
-//cada vez que modifico alguno de los rangos de este panel se deshace lo anterior---------
 
-// una funcion que agrupe todos los rangos y que se ejecute cada vez que cambio alguno, entonces cuando mueve uno tiene que leer como estan todos
+function aplicarFiltros() {
+    $imagenMeme.style.filter = `
+        brightness(${$rangeBrillo.value})
+        opacity(${$rangeOpacidad.value})
+        contrast(${$rangeContraste.value}%)
+        blur(${$rangeDesenfoque.value}px)
+        grayscale(${$rangeGrises.value}%)
+        sepia(${$rangeSepia.value}%)
+        hue-rotate(${$rangeHue.value}deg)
+        saturate(${$rangeSaturado.value}%)
+        invert(${$rangeNegativo.value})
+    `;
+}
 
-// function cambioRango () {
-//     $imagenMeme.style.filter = `brightness(${$rangeBrillo.value})`;
-//     $imagenMeme.style.filter = `opacity(${$rangeOpacidad.value})`;
-//     $imagenMeme.style.filter = `contrast(${$rangeContraste.value})`;
-//     $imagenMeme.style.filter = `blur(${$rangeDesenfoque.value})`;
-//     $imagenMeme.style.filter = `grayscale(${$rangeGrises.value})`;
-//     $imagenMeme.style.filter = `sepia(${$rangeSepia.value})`;
-//     $imagenMeme.style.filter = `hue-rotation(${$rangeHue.value})`;
-//     $imagenMeme.style.filter = `saturation(${$rangeSaturado.value})`;
-//     $imagenMeme.style.filter = `invert(${$rangeNegativo.value})`;
-// }
+$buttonResetFiltros.addEventListener("click", () => {
+    $rangeBrillo.value = "1" 
+    $rangeOpacidad.value = "1"
+    $rangeContraste.value = "100%"
+    $rangeDesenfoque.value = "0px"
+    $rangeGrises.value = "0%" 
+    $rangeSepia.value = "0%"
+    $rangeHue.value = "0deg"
+    $rangeSaturado.value = "100%"
+    $rangeNegativo.value = "1"
+    aplicarFiltros()
+})
 
-// ------FIN DE FILTROS----------------
-
-
-
+// ------FIN DE FILTROS-----
 
 
-// ====== FUNCIONES PANEL TEXTO ======
 
-// INPUT TEXTO SUPERIOR E INFERIOR
+// ========== ASIDES ==========
+
+//= ASIDE TEXTO =
+// INPUT TEXTO SUPERIOR
 $inputTextoSup.addEventListener("input", () => {
     $h2Top.innerText = `${$inputTextoSup.value}`
 })
-
+// INPUT TEXTO INFERIOR
 $inputTextoInf.addEventListener("input", () => {
     $h2Bottom.innerText = `${$inputTextoInf.value}`
 })
-// CHECKBOX SIN TEXTO SUPERIOR E INFERIOR
+// CHECKBOX SIN TEXTO SUPERIOR
 $checkSinTextoSup.addEventListener("change", () => {
     if($checkSinTextoSup.checked) {
         $h2Top.style.display = "none";
     } else {
-        $h2Top.style.display = "flex";
+        $h2Top.style.display = "block";
     }
 })
-
+// CHECKBOX SIN TEXTO INFERIOR
 $checkSinTextoInf.addEventListener("change", () => {
     if($checkSinTextoInf.checked) {
         $h2Bottom.style.display = "none";
     } else {
-        $h2Bottom.style.display = "flex";
+        $h2Bottom.style.display = "block";
     }
 })
 
 // FUENTES
-// hacer una funcion para aplicar todas las fuentes, pero primero como cambio la fuente?
-// function cambiarFuente() {
-//     $h2Top.style.fontFamily = $fuente.value
-// }
-
+// TIPO DE FUENTE
 $selectFuente.addEventListener("change", () => {
-    $h2Top.style.fontFamily = $selectFuente.value
+    $h2Top.style.fontFamily = $selectFuente.value;
+    $h2Bottom.style.fontFamily = $selectFuente.value;
+})
+// TAMAÑO DE FUENTE
+$inputTamañoFuente.addEventListener ("input", () => {
+    $h2Top.style.fontSize = `${$inputTamañoFuente.value}px`
+    $h2Bottom.style.fontSize = `${$inputTamañoFuente.value}px`
+})
+// ALINEACION DE FUENTE
+//izquierda
+$buttonIzquierda.addEventListener("click", () => {
+    $h2Top.style.textAlign = "left";
+    $h2Bottom.style.textAlign = "left";
+})
+//centro
+$buttonCentro.addEventListener("click", () => {
+    $h2Top.style.textAlign = "center";
+    $h2Bottom.style.textAlign = "center";
+})
+//derecha
+$buttonDerecha.addEventListener("click", () => {
+    $h2Top.style.textAlign = "right";
+    $h2Bottom.style.textAlign = "right";
+})
+// COLOR DE FUENTE
+$inputColorFuente.addEventListener("input", () => {
+    $h2Top.style.color = `${$inputColorFuente.value}`
+    $h2Bottom.style.color = `${$inputColorFuente.value}`
+})
+// COLOR FONDO TEXTO
+$inputFondo.addEventListener("input", () => {
+    $h2Top.style.backgroundColor = `${$inputFondo.value}`
+    $h2Bottom.style.backgroundColor = `${$inputFondo.value}`
+})
+// check fondo transparente
+$checkTransparente.addEventListener("change", () => {
+    if($checkTransparente.checked) {
+        $h2Top.style.backgroundColor = `transparent`
+        $h2Bottom.style.backgroundColor = `transparent`
+    } else {
+    $h2Top.style.backgroundColor = `${$inputFondo.value}`
+    $h2Bottom.style.backgroundColor = `${$inputFondo.value}`
+    }
+})
+//CONTORNO
+//ninguno
+$buttonNinguno.addEventListener("click", () => {
+    $h2Top.style.textShadow = "none"
+    $h2Bottom.style.textShadow = "none"
+})
+//claro
+$buttonClaro.addEventListener("click", () => {
+    $h2Top.style.textShadow = "2px 2px 0px white, -2px 2px 0px white, 2px -2px 0px white, -2px -2px 0px white"
+    $h2Bottom.style.textShadow = "2px 2px 0px white, -2px 2px 0px white, 2px -2px 0px white, -2px -2px 0px white"
+})
+//oscuro
+$buttonOscuro.addEventListener("click", () => {
+    $h2Top.style.textShadow = "2px 2px 0px black, -2px 2px 0px black, 2px -2px 0px black, -2px -2px 0px black"
+    $h2Bottom.style.textShadow = "2px 2px 0px black, -2px 2px 0px black, 2px -2px 0px black, -2px -2px 0px black"
+})
+//ESPACIADO
+$inputEspaciado.addEventListener("input", () => {
+    $h2Top.style.padding = `${$inputEspaciado.value}px 0`
+    $h2Bottom.style.padding = `${$inputEspaciado.value}px 0`
+})
+// INTERLINEADO
+$selectInterlineado.addEventListener("change", () => {
+    $h2Top.style.lineHeight = $selectInterlineado.value;
+    $h2Bottom.style.lineHeight = $selectInterlineado.value;
 })
 
-// $inputTamañoFuente.addEventListener ("input", () => {
-//     $h2Top.style.fontSize = `${$inputTamañoFuente.value}`
-// })
 
-
-// // --- TAMAÑO DE FUENTE
-// const $inputTamañoFuente = $("#inputTamaño");
-// // --- ALINEACION DE FUENTE
-// const $buttonIzquierda = $("#izquierda");
-// const $buttonCentro = $("#centro");
-// const $buttonDerecha = $("#derecha");
-
-
-
-
-
-
-
-
-
-
+// ========== FIN ASIDES ============
 
 
 
@@ -300,21 +372,3 @@ $selectFuente.addEventListener("change", () => {
 //     }
 // })
 
-/*  FORMA 1 */
-// const titulo = prompt('ingrese un titulo');
-// const url = prompt('ingrese la url de imagen');
-// const urlArticul = prompt('ingrese la url del articulo')
-
-// const titulo1 = document.querySelector('#titulo')
-// titulo1.innerText = titulo;
-// const url1 = document.querySelector('#img')
-// url1.src = url
-// const urlArticul1 = document.querySelector('#url')   // <a id='url'>Ver mas...</a>
-// urlArticul1.href = urlArticul;
-
-// const card = document.querySelector('#card');
-// card.style.border = "2px solid black";
-// card.style.boxShadow = "0 4px 12px black";
-// card.style.fontFamily = "arial";
-// card.style.fontSize = "30px";
-// card.style.color = "red";
